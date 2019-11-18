9952fc48a308f137ece6314f09173754bfcf0495
AleeSeydoux <aleevelarde9@gmail.com>
2019-11-17 22:26:11 -0600

    get our elements

--- D11/scripts.js
+++ D11/scripts.js
@@ -1,7 +1,13 @@
 /*Get our elements */
 
 const player = document.querySelector('.player');
-const 
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progress_bar = querySelector('.progress__filled');
+const toggle = querySelector('.toggle');
+const skipButtons = querySelectorAll('[data-skip]');
+const ranger = querySelectorAll('.player__slider');
 /*build our functions */
+
 
 /*Hook up the event listener */
